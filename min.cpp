6ee753c08bb34738e415e2707299e0fa286bb6bb
MatiBiela <mateuszbiela9@gmail.com>
2020-05-08 08:49:20 +0200

    my commit

---
+++ min.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int i[10];
+	int min;
+	
+ plik.open("C:\\zadanko\\a.txt");  
+    if(plik.good())  
+	 while(!plik.eof())  
+					{
+                    plik>>t[i++] ; 
+                   	 cout<<endl; 
+                    }
+    plik.close();
+	
+    min = tab[0];
+	
+	for(int i=1;i<10;i++) 
+		if(min>tab[i])
+			min = tab[i];
+        
+    int i=0
+    plik.open("c:\\zadanko\\a.txt")
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
